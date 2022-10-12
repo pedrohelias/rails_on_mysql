f0e89657546760cd53f46134283d7b4f62b8144b
pedrohelias <pedrohelias95@hotmail.com>
2022-10-12 19:36:38 -0300

    grande evolucao em rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  root "welcome#index"
+
   resources :customers
+
+  get "inicio" => "welcome#index"
+
+  #podemos tambem criar uma porta padrão para pegar a home, com a root. Caso eu naõ digite nada, ele vai sempre cair inicialmente no mesmo local
+
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
