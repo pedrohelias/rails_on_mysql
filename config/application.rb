a801ca601449a360e94ddbdd5f664e592682d2cd
pedrohelias <pedrohelias95@hotmail.com>
2022-10-18 15:12:42 -0300

    i18n

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module ProjetoPedro
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    ##config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,5 +18,6 @@ module ProjetoPedro
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = "pt-BR"
   end
 end
